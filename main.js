cf4d1f8068ebce4f8cb4f59e24081a37e99b55c8
MortyDevelop <166936500+MortyDevelop@users.noreply.github.com>
2024-04-24 00:39:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const container = document.getElementById("container");
 const imageOne = document.querySelector(".image-1");
 const imageTwo = document.querySelector(".image-2");
@@ -6,36 +5,33 @@ const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
 
 function getRandomNumber(min, max) {
-  // Calculate the random number between min and max (inclusive)
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return randomNumber;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-btnNo.addEventListener("click", (event) => {
+function repositionBtnNo() {
   const containerHeight = container.getBoundingClientRect().height;
   const containerWidth = container.getBoundingClientRect().width;
   const btnHeight = btnNo.getBoundingClientRect().height;
   const btnWidth = btnNo.getBoundingClientRect().width;
-  const btnTop = btnNo.getBoundingClientRect().top;
-  const btnLeft = btnNo.getBoundingClientRect().left;
 
-  let newTop = btnTop;
-  let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-    newTop = getRandomNumber(0, containerHeight - btnHeight);
-  }
+  let newTop = getRandomNumber(0, containerHeight - btnHeight);
+  let newLeft = getRandomNumber(0, containerWidth - btnWidth);
 
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-    newLeft = getRandomNumber(0, containerWidth - btnWidth);
-  }
+  btnNo.style.top = `${newTop}px`;
+  btnNo.style.left = `${newLeft}px`;
+}
 
-  btnNo.style.top = Math.floor(newTop) + "px";
-  btnNo.style.left = Math.floor(newLeft) + "px";
+btnNo.addEventListener("click", (event) => {
+  repositionBtnNo();
 });
 
 btnYes.addEventListener("click", (e) => {
   btnNo.classList.add("hide");
   imageOne.classList.add("hide");
   imageTwo.classList.remove("hide");
+});
+
+// Reposition btnNo when the window is resized
+window.addEventListener("resize", () => {
+  repositionBtnNo();
 });
